67633e4392b788ca236283ea424acd5f4109dcdb
Amy Langa <amyl@havenly.com>
2016-03-17 18:46:11 -0500

    Fix cross browser responsiveness issues.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready( function() {
 
+	adjustContainers();
+	$(window).resize(function(){
+	    adjustContainers();
+	});
+	
 	$('.nav-links a').click( function(e) {
 		e.preventDefault();
 		var sectionId = $(this).attr('href');
@@ -7,3 +12,15 @@ $(document).ready( function() {
 	})
 
 });
+
+function adjustContainers() {
+	
+	var width = $(window).width();
+	var height = $(window).height();
+	$('.intro, .right-section, .left-section').css('height', height);
+	
+	var sectionHeight = width > 768 ? height : $('.right-section').outerHeight() + $('.left-section').outerHeight();
+	$('.section').css('height', sectionHeight);
+	$('.left-section .title').css('padding-top', + height / 3 + "px");
+	$('.intro').css('padding-top', + height / 10 + "px");
+}
